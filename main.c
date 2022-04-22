2e9fd9918aec8aff84b4bbeb54058b74771306d2
David Siaw <davidsiaw@gmail.com>
2022-04-22 12:31:15 +0900

    initial

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main()
+{
+	uint LED_PIN = PICO_DEFAULT_LED_PIN;
+	
+	stdio_init_all();
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	
+	int state = 0;
+	while (true)
+	{
+		printf("blink\n");
+		state = (state + 1) % 2;
+		gpio_out(LED_PIN, state);
+		sleep_ms(250)
+	}
+	return 0;
+}
+
